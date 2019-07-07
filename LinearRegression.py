7f7ed6891ad1e212369b1017ea13949c2627d973
Prahul345 <pandeyrahul345@gmail.com>
2019-07-07 09:50:58 +0530

    Add files via upload
    changes done succesfully

---
+++ LinearRegression.py
@@ -0,0 +1,42 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+import os
+print(os.listdir("../input"))
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+#Importing the dataset
+dataset = pd.read_csv('../input/Salary_Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
+
+#splitting the dataset into Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.3, random_state = 0)
+
+#Fitting simple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = regressor.predict(X_test)
+#Visualising the training set result
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show
+
+# Visualising the test set result
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show
